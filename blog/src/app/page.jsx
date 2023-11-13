b4096ef642fe9cbc16175cd24c099523992d9dca
MLDODSON <paola@dominicansoul.com>
2023-11-13 12:07:29 -0600

    Branch 1
    Blog project basic setup and layout.

---
+++ blog/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return <div>   
+      <Link href="/">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Erat nam at lectus urna. Risus commodo viverra maecenas accumsan lacus vel facilisis. Sem fringilla ut morbi tincidunt augue interdum velit euismod in.
+      </Link>
+    </div>;
+}
